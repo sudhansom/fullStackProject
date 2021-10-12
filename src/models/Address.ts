56dce7e9b4dbe2343c446ed4d5115533295ba7da
bhuwan <bkspoudel@gmail.com>
2021-10-12 06:41:03 +0200

    Address Model created

---
+++ src/models/Address.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document } from 'mongoose'
+
+export type AddressDocument = Document & {
+  street: string
+  houseNo: number
+  postalCode: number
+  city: string
+  country: string
+}
+
+const addressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    required: true,
+  },
+  houseNo: {
+    type: Number,
+    required: true,
+  },
+  postalCode: {
+    type: Number,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+})
+
+export default mongoose.model<AddressDocument>('Address', addressSchema)
