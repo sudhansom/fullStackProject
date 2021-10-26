9066c5083f315c4323c22e6e96e1ebdae0314d95
bhuwan <bkspoudel@gmail.com>
2021-10-26 13:31:52 +0200

    modified detail page

--- client/src/components/DetailPage.tsx
+++ client/src/components/DetailPage.tsx
@@ -1,11 +1,66 @@
 import React from 'react'
 
+import { Button, Card, Container, Form, Row, Col } from 'react-bootstrap'
+import image from '../images/abc.png'
+
 function DetailPage() {
+    const addToCart = (e: React.MouseEvent<HTMLButtonElement>)=>{
+        console.log("clicked one")
+    }
     return (
-        <div className="App">
-            <h1>Details.....</h1>
+        
+        <div>
+            <div className="card_details">
+                    <a href="/details/"><img src={image} alt="Nature"   width="100%"/></a>
+                    
+            </div>
+            <div className="card_details">
+                    <div>
+                        <a href="/details/"><img src={image} alt="Nature"   width="20%"/></a>
+                        <a href="/details/"><img src={image} alt="Nature"   width="20%"/></a>
+                        <a href="/details/"><img src={image} alt="Nature"   width="20%"/></a>
+                    </div>
+                    
+                    <div>
+                        <h3>Select your match: </h3>
+                        <Form>
+                            <Form.Group>
+                                <Row>
+                                    <Col>
+                                    <h5>Company</h5>
+                                        <select id="brand" className="variant" >
+                                            <option value="ios">ios</option>
+                                            <option value="mk" >mk</option>
+                                            <option value="android">android</option>
+                                        </select>
+                                    </Col>
+                                    <Col>
+                                    <h5>Color</h5>
+                                        <select id="color" className="variant" >
+                                            <option value="red">red</option>
+                                            <option value="blue" >blue</option>
+                                            <option value="green">green</option>
+                                        </select>
+                                    </Col>
+                                    <Col>
+                                        <h5>Size</h5>
+                                        <select id="size" className="variant" >
+                                            <option value="small">small</option>
+                                            <option value="medium" >medium</option>
+                                            <option value="large">large</option>
+                                        </select>
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+                        
+                        </Form> 
+                    </div>
+                    <Button variant="primary" onClick={(e)=>{addToCart(e)}}>Add to Cart</Button>     
+            </div>
+            
         </div>
     )
 }
 
 export default DetailPage
+
--- client/src/components/HeaderPart.tsx
+++ client/src/components/HeaderPart.tsx
@@ -6,7 +6,7 @@ function HeaderPart() {
     return (
         <div className="header">
             
-            <img src={image} alt=""  width="100%" height="90%"/>
+            <img src={image} alt=""  width="100%" height="150px"/>
         </div>
     )
 }
