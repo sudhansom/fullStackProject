b57bd91bf6ed5fdb30f30cce029c8bb749a95537
bhuwan <bkspoudel@gmail.com>
2021-10-29 12:40:56 +0200

    detailpage relate image

--- client/src/components/DetailPage.tsx
+++ client/src/components/DetailPage.tsx
@@ -4,6 +4,9 @@ import { ProductDocument } from '../../../src/models/Product'
 
 import { Button, Card, Container, Form, Row, Col } from 'react-bootstrap'
 import image from '../images/abc.png'
+import watch1 from '../images/watch1.png'
+import watch2 from '../images/watch2.png'
+import watch3 from '../images/watch3.png'
 import axios from 'axios'
 
 
@@ -12,6 +15,7 @@ type Params = {
 }
 
 function DetailPage() {
+    const [picture, setPicture] = useState(image)
     const [product, setProduct] = useState<ProductDocument | null>(null)
     const { productId } = useParams<Params>()
     const addToCart = (e: React.MouseEvent<HTMLButtonElement>)=>{
@@ -23,23 +27,36 @@ function DetailPage() {
     }, [])
     
     console.log(product)
+
+    const changePicture = ()=> {
+        console.log("clicked picture....")
+        setPicture(watch1)
+    }
            
     return (
         
-            <div>
-            <div className="card_details">
-                    <a href="/"><img src={image} alt="Nature"   width="100%"/></a>
-                    
-            </div>
-            <div className="card_details">
-                    <div>
-                        <a href="/"><img src={image} alt="Nature"   width="20%"/></a>
-                        <a href="/"><img src={image} alt="Nature"   width="20%"/></a>
-                        <a href="/"><img src={image} alt="Nature"   width="20%"/></a>
+            <div className="detail">
+                <div className="side_details">
+                    <div className="each_photo">
+                        <img src={watch1} alt="Nature"   width="100%" onClick={changePicture}/>
                     </div>
-                    
-                    <div>
-                        <h3>Select your match: </h3>
+                    <hr />
+                    <div className="each_photo">
+                        <a href="/"><img src={watch2} alt="Nature"   width="100%"/></a> 
+                    </div>
+                    <hr />
+                    <div className="each_photo">
+                        <a href="/"><img src={watch3} alt="Nature"   width="100%"/></a> 
+                    </div>
+                    <hr />
+               
+                </div>
+                <div className="card_details" >
+                    <a href="/"><img src={picture} alt="Nature"   width="100%"/></a>
+                     <div>
+                         <hr />
+                         <hr />
+                        
                         <Form>
                             <Form.Group>
                                 <Row>
@@ -72,8 +89,18 @@ function DetailPage() {
                         
                         </Form> 
                     </div>
-                    <Button variant="primary" onClick={(e)=>{addToCart(e)}}>Add to Cart</Button>     
+                    
             </div>
+            {/* <div className="card_details">
+                    <div>
+                        <a href="/"><img src={image} alt="Nature"   width="20%"/></a>
+                        <a href="/"><img src={image} alt="Nature"   width="20%"/></a>
+                        <a href="/"><img src={image} alt="Nature"   width="20%"/></a>
+                    </div>
+                    
+                   
+                    <Button variant="primary" onClick={(e)=>{addToCart(e)}}>Add to Cart</Button>     
+            </div> */}
         </div>
        
     )
