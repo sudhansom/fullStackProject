0be820ca7a37d7b65333f3cfc9fb39f3131881c0
bhuwan <bkspoudel@gmail.com>
2021-11-02 23:56:38 +0100

    footer part changed

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import {Store} from '../redux/reducers'
 import logo from '../images/logo.png'
+import 'font-awesome/css/font-awesome.min.css'
 
 function Footer() {
+    const isLoggedIn = useSelector((state: Store)=>state.userReducer.isLoggedIn)
     return (
         <div className="footer">
-            <ul className="leftSide">
-                <li><a href="#"><img src={logo} height="30px" width="40px"></img></a></li>
-                <li><a href="/">Home</a></li>
-                <li><a href='/products/'>Products</a></li>
+            <ul className="leftSide1">
+                <li><i className="fa fa-spinner fa-spin"></i></li>
+                <li><i className="fa fa-twitter fa-stack-1x"></i></li>
+                {/* <i className="fa fa-shopping-cart" aria-hidden="true"></i> */}
+                <li style={{display: isLoggedIn?'inline-block': 'none', marginLeft: '1em'}}><a href='/'><i className="fa fa-unlock" ></i></a></li>
+                <li style={{display: isLoggedIn?'none': 'inline-block', marginLeft: '1em'}}><a href='/'><i className="fa fa-lock"></i></a></li>
+                
             </ul>
             <ul className="rightSide">
-                <li><a href="/addProduct/">AddProduct</a></li>
-                <li>Logout</li>
-                <li>Cart</li>
+                <li>&copy; CopyRight 2021</li>
             </ul>
+            
             
         </div>
     )
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import { useSelector } from 'react-redux'
+import {Store} from '../redux/reducers'
 import logo from '../images/logo.png'
 
 import cart from '../images/cart.png'
@@ -7,7 +9,7 @@ import { Dropdown } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Navbar() {
-    //const isLoggedIn = useSelector(state=>state.userReducer.isLoggedIn) 
+    const isLoggedIn = useSelector((state: Store)=>state.userReducer.isLoggedIn) 
     const clearLocalStorage = () => {
         localStorage.removeItem('token')
         localStorage.removeItem('user')
@@ -47,8 +49,8 @@ function Navbar() {
                     </Dropdown>
             </div>
             <ul className="rightSide">
-                <li style={{display: localStorage.getItem('token')? 'none': 'block'}}><a href="/login/">Login</a></li>
-                <li style={{display: localStorage.getItem('token')? 'block': 'none'}}><a href="#" onClick={clearLocalStorage} >Logout</a></li>
+                <li style={{display: isLoggedIn? 'none': 'block'}}><a href="/login/">Login</a></li>
+                <li style={{display: isLoggedIn? 'block': 'none'}}><a href="#" onClick={clearLocalStorage} >Logout</a></li>
                 <div className="cart" >
                     <li><a href='/cartPage'><img className="cartImage" src={cart} width="50px" height="40px"></img></a></li>
                     <li><a href='/cartPage'style={{fontSize: "2em", color:"red", margin:".3em"}}>{total}</a></li>
