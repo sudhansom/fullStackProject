682f8fbfa7a7d830f13fed2922d0003f813f51ea
bhuwan <bkspoudel@gmail.com>
2022-02-04 11:07:59 +0100

    deleted yarn.lock

--- client/src/components/Bitcoin.tsx
+++ client/src/components/Bitcoin.tsx
@@ -3,10 +3,10 @@ import axios from 'axios'
 import cors from 'cors'
 
 function Bitcoin() {
-    // const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
-    // const qString = "?CMC_PRO_API_KEY=cce87ebd-aca9-4f37-b822-75a972468e5a" + "&start=1&limit=5&convert=USD";
+    const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+    const qString = "?CMC_PRO_API_KEY=cce87ebd-aca9-4f37-b822-75a972468e5a" + "&start=1&limit=5&convert=USD";
 
-    const result = axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest')
+    const result = axios.get(url + qString)
     console.log(result)
 
     return (
