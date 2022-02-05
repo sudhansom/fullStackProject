f0c1a06ecb3478d1809421cf6e11f2b8608e08be
bhuwan <bkspoudel@gmail.com>
2022-02-05 12:34:43 +0100

    deleted jwt-decode'

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -6,12 +6,6 @@ import {useDispatch, useSelector} from "react-redux"
 import { Button, Card, Container, Form, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import image from '../images/abc.png'
-// import jwt_decode from 'jwt-decode'
-// import { GoogleLogin } from 'react-google-login';
-// import axios from 'axios'
-
-// import { getUser } from '../redux/action'
 
 type Response = {
   token: string
