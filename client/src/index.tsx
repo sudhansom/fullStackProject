be090c1b3307a1ae8fd6c1269b04e722687fdbc5
bhuwan <bkspoudel@gmail.com>
2022-02-15 23:11:09 +0100

    comment in index.tsx for tomorrow work

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import axios from 'axios'
 
 import { Provider } from 'react-redux'
 
 import storeFactory from './redux/store'
 
-axios.interceptors.request.use(request => {
+//set Base urls for axios here....
+
+axios.interceptors.request.use((request) => {
   const token = localStorage.getItem('token') as string
-  console.log("-----------**********  am i executed ??  *********-------------------------------")
+  console.log(
+    '-----------**********  am i executed ??  *********-------------------------------'
+  )
   const requestHeader = {
-    Authorization: `Bearer ${token}`
+    Authorization: `Bearer ${token}`,
   }
   request.headers = requestHeader
   return request
@@ -22,18 +26,16 @@ axios.interceptors.request.use(request => {
 const reduxStore = storeFactory()
 ReactDOM.render(
   <React.StrictMode>
-    
-     <Provider store={reduxStore}>
-        <Router>
+    <Provider store={reduxStore}>
+      <Router>
         <App />
-        </Router>
-      
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
