79ed2bb2f178dcd8536bae98105e165b9b7ca0d9
bhuwan <bkspoudel@gmail.com>
2022-02-27 18:25:26 +0100

    try to find where the error is in detailPage.... not getting oneProduct

--- client/src/components/DetailPage.tsx
+++ client/src/components/DetailPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import {useDispatch, useSelector} from "react-redux"
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Button, Card, Container, Form, Row, Col } from 'react-bootstrap'
 import image from '../images/abc.png'
@@ -10,137 +10,173 @@ import { getProduct, getOneProduct } from '../redux/action'
 import { ProductDocument } from '../../../src/models/Product'
 import { Store } from '../redux/reducers'
 
-
 type Params = {
-    productId: string
+  productId: string
 }
-type VariantDocument =  {
+type VariantDocument = {
   [key: string]: string
 }
 
-
 function DetailPage() {
-    const dispatch = useDispatch()
-    const [picture, setPicture] = useState(image)
-    const [product, setProduct] = useState<ProductDocument | null>(null)
-    const { productId } = useParams<Params>()
-    const oneProduct = useSelector((state: Store) => state.productReducer.oneProduct)
+  const dispatch = useDispatch()
+  const [picture, setPicture] = useState(image)
+  const [product, setProduct] = useState<ProductDocument | null>(null)
+  const { productId } = useParams<Params>()
+  console.log('productId:', productId)
+  const oneProduct = useSelector(
+    (state: Store) => state.productReducer.oneProduct
+  )
 
-    useEffect(()=>{
-        dispatch(getOneProduct(productId))
-    }, [productId])
-    console.log('oneProduct....',oneProduct)
-    const [variant, setVariant] = useState<VariantDocument>({
-        brand: "ios",
-        size: "red",
-        color: "small"
-    })
-    
-    console.log(product)
+  useEffect(() => {
+    console.log('inside useEffect')
+    dispatch(getOneProduct(productId))
+  }, [productId])
+  console.log('oneProduct....', oneProduct)
+  const [variant, setVariant] = useState<VariantDocument>({
+    brand: 'ios',
+    size: 'red',
+    color: 'small',
+  })
 
-    const changePicture = ()=> {
-        console.log("clicked picture....")
-       // setPicture(watch1)
-    }
-    const goBack = (e: React.MouseEvent<HTMLButtonElement>)=>{
-        //eslint-disable-next-line
-        location.href = "/products"
-    }
+  console.log(product)
 
-    const addToCart = (e: React.MouseEvent<HTMLButtonElement>)=>{
-        console.log("clicked one")
-        dispatch(getProduct(productId, variant))
-        alert("product added to the cart")
-    }
-    const updateVariants = (e: React.ChangeEvent<HTMLSelectElement>, val: string) => {
-        const values = {...variant}
-        values[val] = e.target.value
-        setVariant(values)
-    }
+  const changePicture = () => {
+    console.log('clicked picture....')
+    // setPicture(watch1)
+  }
+  const goBack = (e: React.MouseEvent<HTMLButtonElement>) => {
+    //eslint-disable-next-line
+    location.href = '/products'
+  }
 
-    // for (let i=0; i<oneProduct.images.length; i++){
-    //     let itemName = 'name' + i.toString()
-    //     itemName = oneProduct.images[i]
-    // }
-        
-           
-    return (
-        
-            <div className="detail">
-                <div className="side_details">
-                    <div className="each_photo">
-                        <img src={oneProduct.images[0]} alt="Nature"   width="100%" onClick={changePicture}/>
-                    </div>
-                    <hr />
-                    <div className="each_photo">
-                        <a href="#"><img src={oneProduct.images[0]} alt="Nature"   width="100%"/></a> 
-                    </div>
-                    <hr />
-                    <div className="each_photo">
-                        <a href="#"><img src={oneProduct.images[0]} alt="Nature"   width="100%"/></a> 
-                    </div>
-                    <hr />
-               
-                </div>
-                <div className="card_details" >
-                    <a href="#"><img src={oneProduct.images[0]} alt="Nature"   width="100%"/></a>
-                     <div>
-                         <hr />
-                        
-                        <Form>
-                            <Form.Group>
-                                <Row>
-                                    <Col>
-                                    <h5>Company</h5>
-                                    <select id="brand" className="variant" onChange={(e: React.ChangeEvent<HTMLSelectElement>)=>{updateVariants(e, 'brand' )}} value={variant.brand}>
-                                            <option value="ios">ios</option>
-                                            <option value="mk" >mk</option>
-                                            <option value="android">android</option>
-                                        </select>
-                                    </Col>
-                                    <Col>
-                                    <h5>Color</h5>
-                                    <select id="color" className="variant" onChange={(e: React.ChangeEvent<HTMLSelectElement>)=>{updateVariants(e, 'color' )}} value={variant.color}>
-                                            <option value="red">red</option>
-                                            <option value="blue" >blue</option>
-                                            <option value="green">green</option>
-                                        </select>
-                                    </Col>
-                                    <Col>
-                                        <h5>Size</h5>
-                                        <select id="size" className="variant" onChange={(e: React.ChangeEvent<HTMLSelectElement>)=>{updateVariants(e, 'size' )}} value={variant.size}>
-                                            <option value="small">small</option>
-                                            <option value="medium" >medium</option>
-                                            <option value="large">large</option>
-                                        </select>
-                                    </Col>
-                                </Row>
-                            </Form.Group>
-                        
-                        </Form> 
-                    </div>
-                    <Button variant="primary" onClick={goBack} style={{marginRight: "2em"}}>&larr; Back</Button>
-                    <Button variant="primary" onClick={addToCart}>Add to Cart</Button>  
-                    
-                    
-            </div>
-            <div className="card_detail">
-                    <div>
-                        <h3 style={{marginLeft: "5em"}}>Detail of the item</h3>
-                        <hr />
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi illo mollitia quam expedita sunt fuga dolorem consectetur quae nulla dolores. Doloribus, iure. Culpa cum repellendus accusantium molestias rem, veniam ex!</p>
+  const addToCart = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('clicked one')
+    dispatch(getProduct(productId, variant))
+    alert('product added to the cart')
+  }
+  const updateVariants = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+    val: string
+  ) => {
+    const values = { ...variant }
+    values[val] = e.target.value
+    setVariant(values)
+  }
 
-                    </div>
-                    
+  // for (let i=0; i<oneProduct.images.length; i++){
+  //     let itemName = 'name' + i.toString()
+  //     itemName = oneProduct.images[i]
+  // }
 
-                    
-                   
-                         
-            </div>
+  return (
+    <div className="detail">
+      <div className="side_details">
+        <div className="each_photo">
+          <img
+            src={oneProduct.images[0]}
+            alt="Nature"
+            width="100%"
+            onClick={changePicture}
+          />
         </div>
-       
-    )
+        <hr />
+        <div className="each_photo">
+          <a href="#">
+            <img src={oneProduct.images[0]} alt="Nature" width="100%" />
+          </a>
+        </div>
+        <hr />
+        <div className="each_photo">
+          <a href="#">
+            <img src={oneProduct.images[0]} alt="Nature" width="100%" />
+          </a>
+        </div>
+        <hr />
+      </div>
+      <div className="card_details">
+        <a href="#">
+          <img src={oneProduct.images[0]} alt="Nature" width="100%" />
+        </a>
+        <div>
+          <hr />
+
+          <Form>
+            <Form.Group>
+              <Row>
+                <Col>
+                  <h5>Company</h5>
+                  <select
+                    id="brand"
+                    className="variant"
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                      updateVariants(e, 'brand')
+                    }}
+                    value={variant.brand}
+                  >
+                    <option value="ios">ios</option>
+                    <option value="mk">mk</option>
+                    <option value="android">android</option>
+                  </select>
+                </Col>
+                <Col>
+                  <h5>Color</h5>
+                  <select
+                    id="color"
+                    className="variant"
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                      updateVariants(e, 'color')
+                    }}
+                    value={variant.color}
+                  >
+                    <option value="red">red</option>
+                    <option value="blue">blue</option>
+                    <option value="green">green</option>
+                  </select>
+                </Col>
+                <Col>
+                  <h5>Size</h5>
+                  <select
+                    id="size"
+                    className="variant"
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                      updateVariants(e, 'size')
+                    }}
+                    value={variant.size}
+                  >
+                    <option value="small">small</option>
+                    <option value="medium">medium</option>
+                    <option value="large">large</option>
+                  </select>
+                </Col>
+              </Row>
+            </Form.Group>
+          </Form>
+        </div>
+        <Button
+          variant="primary"
+          onClick={goBack}
+          style={{ marginRight: '2em' }}
+        >
+          &larr; Back
+        </Button>
+        <Button variant="primary" onClick={addToCart}>
+          Add to Cart
+        </Button>
+      </div>
+      <div className="card_detail">
+        <div>
+          <h3 style={{ marginLeft: '5em' }}>Detail of the item</h3>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi illo
+            mollitia quam expedita sunt fuga dolorem consectetur quae nulla
+            dolores. Doloribus, iure. Culpa cum repellendus accusantium
+            molestias rem, veniam ex!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default DetailPage
-
--- client/src/redux/action.tsx
+++ client/src/redux/action.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import axios from 'axios'
-import {Store} from '../redux/reducers/index'
+import { Store } from '../redux/reducers/index'
 import { useSelector, useDispatch } from 'react-redux'
 
-import {ProductDocument} from '../../../src/models/Product'
-import {UserDocument} from '../../../src/models/Users'
+import { ProductDocument } from '../../../src/models/Product'
+import { UserDocument } from '../../../src/models/Users'
 
 //import {VariantDocument} from '../../../src/models/Variant'
 
@@ -12,156 +12,169 @@ type Fields = {
   [key: string]: string
 }
 
-export const getProduct =  (productId: string, variants: Fields) => {
-    console.log('inside thunk...')
-   return async (dispatch: any, getState: any) => {
-        try{
-            //eslint-disable-next-line
-            let productList = localStorage.getItem('product')?(JSON.parse(localStorage.getItem('product') as string)):[]
-            const product = await axios.get<any>(`http://localhost:5000/api/v1/products/${productId}`)
-            console.log('product: ', product.data)
-            product.data.variant = [variants]
-            
-            localStorage.setItem('product', JSON.stringify([...productList, product.data]))
-            dispatch(successProduct([...productList, product.data]))
-        }catch(err){
-            dispatch(onErrorProduct(err))
-        }
-   }
+export const getProduct = (productId: string, variants: Fields) => {
+  console.log('inside thunk...')
+  return async (dispatch: any, getState: any) => {
+    try {
+      //eslint-disable-next-line
+      let productList = localStorage.getItem('product')
+        ? JSON.parse(localStorage.getItem('product') as string)
+        : []
+      const product = await axios.get<any>(
+        `http://localhost:5000/api/v1/products/${productId}`
+      )
+      console.log('product: ', product.data)
+      product.data.variant = [variants]
+
+      localStorage.setItem(
+        'product',
+        JSON.stringify([...productList, product.data])
+      )
+      dispatch(successProduct([...productList, product.data]))
+    } catch (err) {
+      dispatch(onErrorProduct(err))
+    }
+  }
 }
 
 export const successProduct = (product: ProductDocument[]) => {
-    return {
-        type: "PRODUCT_SUCCESS",
-        payload: product,
-    }
+  return {
+    type: 'PRODUCT_SUCCESS',
+    payload: product,
+  }
 }
 
-export const getUser = (url: string, fields: Fields ) => {
-    return async (dispatch: any, getState: any) => {
-        try{
-            const user = await axios.post<any>(url, fields)
-            localStorage.setItem('token', JSON.stringify(user.data.token))
-            localStorage.setItem('user', JSON.stringify(user.data.userData))
-            localStorage.setItem('isLoggedIn', JSON.stringify(true))
-            dispatch(successUser(user.data.userData))
-        }catch(err){
-            dispatch(onErrorUser(err))
-        }
+export const getUser = (url: string, fields: Fields) => {
+  return async (dispatch: any, getState: any) => {
+    try {
+      const user = await axios.post<any>(url, fields)
+      localStorage.setItem('token', JSON.stringify(user.data.token))
+      localStorage.setItem('user', JSON.stringify(user.data.userData))
+      localStorage.setItem('isLoggedIn', JSON.stringify(true))
+      dispatch(successUser(user.data.userData))
+    } catch (err) {
+      dispatch(onErrorUser(err))
     }
+  }
 }
 export const successUser = (user: UserDocument) => {
-    return {
-        type: "USER_SUCCESS",
-        payload: user,
-    }
+  return {
+    type: 'USER_SUCCESS',
+    payload: user,
+  }
 }
 
-
 export const onErrorUser = (err: any) => {
-    return {
-        type: "ON_ERROR_USER",
-        payload: err,
-    }
+  return {
+    type: 'ON_ERROR_USER',
+    payload: err,
+  }
 }
 
 export const onErrorProduct = (err: any) => {
-    return {
-        type: "ON_ERROR_PRODUCT",
-        payload: err,
-    }
+  return {
+    type: 'ON_ERROR_PRODUCT',
+    payload: err,
+  }
 }
 
 export const getOrder = (order: any) => {
-    // let orderList = localStorage.getItem('order')?(JSON.parse(localStorage.getItem('order') as string)):[]
-    // orderList = [...orderList, order]
-    localStorage.setItem('order', JSON.stringify(order))
+  // let orderList = localStorage.getItem('order')?(JSON.parse(localStorage.getItem('order') as string)):[]
+  // orderList = [...orderList, order]
+  localStorage.setItem('order', JSON.stringify(order))
 
-    return {
-        type: "SAVE_ORDER",
-        payload: order
-    }
+  return {
+    type: 'SAVE_ORDER',
+    payload: order,
+  }
 }
 
 export const getOneProduct = (productId: string) => {
-    return async (dispatch: any, getState: any ) => {
-        try{
-            const url = `http://localhost:5000/api/v1/products/${productId}`
-            const result = await axios.get(url)
-            const oneProduct = result.data as ProductDocument
-            localStorage.setItem('oneProduct', JSON.stringify(oneProduct))
-            dispatch(successOneProduct(oneProduct))
-        }catch(err){
-            dispatch(failureOneProduct(err))
-        }
+  return async (dispatch: any, getState: any) => {
+    console.log('in action:', productId)
+    try {
+      const url = `http://localhost:5000/api/v1/products/${productId}`
+      const result = await axios.get(url)
+      const oneProduct = result.data as ProductDocument
+      localStorage.setItem('oneProduct', JSON.stringify(oneProduct))
+      dispatch(successOneProduct(oneProduct))
+    } catch (err) {
+      dispatch(failureOneProduct(err))
     }
+  }
 }
 export const successOneProduct = (oneProduct: ProductDocument) => {
-    return {
-        type: "SUCCESS_ONE_PRODUCT",
-        payload: oneProduct,
-    }
+  return {
+    type: 'SUCCESS_ONE_PRODUCT',
+    payload: oneProduct,
+  }
 }
 export const failureOneProduct = (err: any) => {
-    return {
-        type: "FAILURE_ONE_PRODUCT",
-        payload: err,
-    }
+  return {
+    type: 'FAILURE_ONE_PRODUCT',
+    payload: err,
+  }
 }
 
 export const saveOrderToDataBase = (order: any) => {
+  return async (dispatch: any, getState: any) => {
+    try {
+      const userId = getState().userReducer.user._id
+      //order.users.push(userId)
+      const url = `http://localhost:5000/api/v1/users/${userId}`
+      const result = await axios.get<any>(url)
+      console.log('User is after: ---', result.data)
 
-    return async (dispatch: any, getState: any) => {
-        try{
-            const userId = getState().userReducer.user._id
-            //order.users.push(userId)
-            const url = `http://localhost:5000/api/v1/users/${userId}`
-            const result = await axios.get<any>(url)
-            console.log('User is after: ---', result.data)
-            
-            order.users = []
-            order.completed = true
-            order.users.push(userId)
-            console.log('Order saved', order)
-            const createdOrder = await axios.post<any>(`http://localhost:5000/api/v1/orders`, order)
-            console.log('created Order', createdOrder.data._id)
-            const updatedUser = await axios.put(url, {order: [createdOrder.data._id]})
-            console.log('updatedUser:---',updatedUser)
-            if(updatedUser){
-                localStorage.removeItem('product')
-            }
-            dispatch(onErrorSavingToDataBase('err'))
-
-        }catch(err){
-            dispatch(onErrorSavingToDataBase(err))
-        }
+      order.users = []
+      order.completed = true
+      order.users.push(userId)
+      console.log('Order saved', order)
+      const createdOrder = await axios.post<any>(
+        `http://localhost:5000/api/v1/orders`,
+        order
+      )
+      console.log('created Order', createdOrder.data._id)
+      const updatedUser = await axios.put(url, {
+        order: [createdOrder.data._id],
+      })
+      console.log('updatedUser:---', updatedUser)
+      if (updatedUser) {
+        localStorage.removeItem('product')
+      }
+      dispatch(onErrorSavingToDataBase('err'))
+    } catch (err) {
+      dispatch(onErrorSavingToDataBase(err))
     }
+  }
 }
 
 export const onErrorSavingToDataBase = (err: any) => {
-    return {
-        type: "ON_ERROR_PRODUCT",
-        payload: err,
-    }
+  return {
+    type: 'ON_ERROR_PRODUCT',
+    payload: err,
+  }
 }
 
 export const logOutUser = () => {
-    return {
-        type: "LOGOUT_USER"
-    }
+  return {
+    type: 'LOGOUT_USER',
+  }
 }
 
 type GenericAction<T> = {
-    type: T
+  type: T
 }
-type GenericActionWithPayload<T, K> ={
-    type: T
-    payload: K
+type GenericActionWithPayload<T, K> = {
+  type: T
+  payload: K
 }
 
 type LogOutUser = GenericAction<'LOGOUT_USER'>
 type OnErrorSavingToDataBase = GenericActionWithPayload<'ON_ERROR_PRODUCT', any>
-type SuccessOneProduct = GenericActionWithPayload<'SUCCESS_ONE_PRODUCT', ProductDocument>
+type SuccessOneProduct = GenericActionWithPayload<
+  'SUCCESS_ONE_PRODUCT',
+  ProductDocument
+>
 type FailureOneProduct = GenericActionWithPayload<'FAILURE_ONE_PRODUCT', any>
 type GetOrder = GenericActionWithPayload<'SAVE_ORDER', any>
 type OnErrorProduct = GenericActionWithPayload<'ON_ERROR_PRODUCT', any>
@@ -169,12 +182,13 @@ type OnErrorUser = GenericActionWithPayload<'ON_ERROR_USER', any>
 type SuccessProduct = GenericActionWithPayload<'PRODUCT_SUCCESS', any>
 type SuccessUser = GenericActionWithPayload<'USER_SUCCESS', UserDocument>
 
-export type AllActions = 
- SuccessUser |
- LogOutUser |
- OnErrorSavingToDataBase | 
- SuccessOneProduct | 
- FailureOneProduct | 
- GetOrder | 
- OnErrorProduct | OnErrorUser | SuccessProduct
-
+export type AllActions =
+  | SuccessUser
+  | LogOutUser
+  | OnErrorSavingToDataBase
+  | SuccessOneProduct
+  | FailureOneProduct
+  | GetOrder
+  | OnErrorProduct
+  | OnErrorUser
+  | SuccessProduct
